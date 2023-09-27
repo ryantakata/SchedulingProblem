6e090b70cd0b285b3482cd13a7cf4be4ddc1c766
Ryan Takata <ryantakata50@gmail.com>
2023-09-27 05:00:04 -0700

    Extracted times from input list

--- project1_starter.cpp
+++ project1_starter.cpp
@@ -8,27 +8,34 @@
 
 int main()
 {
-    std::ifstream theFile("input.txt");
-    std::vector<std::string> totalSched;
+    std::ifstream theFile("input.txt");                             //Gets the input file
+    std::vector<std::string> unAvail;                               //Vector of unavailable times
     std::string busy1, busy2, work1, work2;
     int duration = 0;
 
-    while(theFile >> busy1 >> work1 >> busy2 >> work2 >> duration)
+    while(theFile >> busy1 >> work1 >> busy2 >> work2 >> duration)  //Reads the input file and puts information into variables
     {
         
+        for(int i = 0; i < busy1.length(); ++i)                     //Gets times from the busy list and puts it into 
+        {                                                           //the total schedule vector.
+            
+            if(isdigit(busy1[i]))                       
+            {
+                unAvail.push_back(busy1.substr(i, 5));   
+                i+=4;
+            }
+        }
+        
 
-        std::cout << "Person 1 is busy during " << busy1 << "\nAnd is clocked in during " << work1 << "\n";
+        //std::cout << work1 << std::endl;
+        /*std::cout << "Person 1 is busy during " << busy1 << "\nAnd is clocked in during " << work1 << "\n";
                   
         std::cout << "Person 2 is busy during " << busy2 << "\nAnd is clocked in during " << work2 << "\n"
-                  << "The meeting will last " << duration << " minutes\n";
+                  << "The meeting will last " << duration << " minutes\n";*/
     }
-
-    for(int i = 0; i < busy1.length(); ++i)         //Gets the digits from the string, but need to figure out how to use those
-    {                                               //digits to subtract from an overall clock
-        if(isdigit(busy1[i]))
-        {
-            std::cout << busy1[i] << ", ";
-        }
+    for(int i = 0; i < unAvail.size(); ++i)                      //Prints out the unavailable times
+    {
+        std::cout << unAvail[i] << " ";
     }
     
     return 0;
